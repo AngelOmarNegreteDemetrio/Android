5b8952a9b3290c75346603656993a37685a34143
AngelOmarNegreteDemetrio <negretedemetrioangel@gmail.com>
2024-09-04 07:49:37 -0600

    greeting

--- app/src/main/java/com/example/prueba/MainActivity.kt
+++ app/src/main/java/com/example/prueba/MainActivity.kt
@@ -4,16 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.prueba.ui.theme.PruebaTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,8 +31,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
+            Column(
+                modifier=Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
+            ){
+                Text(text ="Simple text")
+             ModifierExample()
+            ModifierExample2()
+                ModifierExample3()
+            }
      //Layouts
-            Column {
+           /*Column {
                 Text(text = "First Row")
                 Text(text = "Second Row")
                 Text(text = "Third Row")
@@ -39,7 +60,7 @@ class MainActivity : ComponentActivity() {
 
                 }
                 Greeting(name ="World")
-            }
+            }*/
             }
             }
         }
@@ -63,5 +84,54 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     PruebaTheme {
         Greeting("Android")
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun ModifierExample(){
+    Column(
+        modifier= Modifier
+            .padding(24.dp)
+    ){
+        Text(text="Hello world")
+
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun ModifierExample2(){
+    Column(
+        modifier= Modifier
+            .padding(24.dp)
+            .fillMaxWidth()
+            .clickable(onClick = {clickAction() })
+    ){
+        Text(text="Hello world")
+
+    }
+}
+
+fun clickAction(){
+    println("Column Clicked")
+
+}
+@Preview(showBackground = true)
+@Composable
+fun ModifierExample3(){
+    Column(
+        modifier= Modifier
+            .fillMaxHeight()
+            .padding(16.dp)
+            .background(Color.Red)
+            .border(width = 2.dp, color = Color.Green)
+            .width(200.dp),
+        horizontalAlignment= Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ){
+        Text(text="Hello Item 1")
+        Text(text="Hello Item 2")
+        Text(text="Hello Item 3")
+        Text(text="Hello Item 4")
+        Text(text="Hello Item 5")
     }
 }
