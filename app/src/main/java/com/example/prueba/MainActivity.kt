0001ee15f44274d5e8d93e7a3b59a5dc785ddb9c
AngelOmarNegreteDemetrio <negretedemetrioangel@gmail.com>
2024-09-06 07:55:51 -0600

    imagen android

--- app/src/main/java/com/example/prueba/MainActivity.kt
+++ app/src/main/java/com/example/prueba/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.prueba
 
+import android.graphics.Picture
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -21,9 +24,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.prueba.ui.theme.PruebaTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,10 +48,12 @@ class MainActivity : ComponentActivity() {
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
             ){
-                Text(text ="Simple text")
-             ModifierExample()
-            ModifierExample2()
-                ModifierExample3()
+                CustomText()
+                Picture()
+               // Text(text ="Simple text")
+            // ModifierExample()
+            //ModifierExample2()
+              //  ModifierExample3()
             }
      //Layouts
            /*Column {
@@ -65,7 +78,7 @@ class MainActivity : ComponentActivity() {
             }
         }
 
-    private fun column(function: () -> Unit) {
+    private fun column(function: () -> Unit) { 
 
     }
 
@@ -79,14 +92,14 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PruebaTheme {
         Greeting("Android")
     }
 }
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun ModifierExample(){
     Column(
@@ -97,41 +110,77 @@ fun ModifierExample(){
 
     }
 }
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun ModifierExample2(){
     Column(
         modifier= Modifier
             .padding(24.dp)
             .fillMaxWidth()
-            .clickable(onClick = {clickAction() })
+            .clickable(onClick = { clickAction() })
     ){
         Text(text="Hello world")
 
     }
 }
-
-fun clickAction(){
-    println("Column Clicked")
-
-}
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
-fun ModifierExample3(){
+fun ModifierExample3() {
     Column(
-        modifier= Modifier
+        modifier = Modifier
             .fillMaxHeight()
             .padding(16.dp)
             .background(Color.Red)
             .border(width = 2.dp, color = Color.Green)
             .width(200.dp),
-        horizontalAlignment= Alignment.CenterHorizontally,
+        horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceEvenly
-    ){
-        Text(text="Hello Item 1")
-        Text(text="Hello Item 2")
-        Text(text="Hello Item 3")
-        Text(text="Hello Item 4")
-        Text(text="Hello Item 5")
+    ) {
+        Text(text = "Hello Item 1")
+        Text(text = "Hello Item 2")
+        Text(text = "Hello Item 3")
+        Text(text = "Hello Item 4")
+        Text(text = "Hello Item 5")
+    }}
+    @Preview(showBackground = true)
+    @Composable
+    fun CustomText() {
+        Column {
+            Text(
+                stringResource(R.string.hello_world_text),
+                color = colorResource(R.color.purple_500),
+                fontSize = 28.sp,
+                fontWeight = FontWeight.ExtraBold
+            )
+            val gradientColors = listOf(Color.Cyan, Color.Blue, Color.Red)
+            Text(
+                stringResource(R.string.hello_world_text),
+                style = TextStyle(brush = Brush.linearGradient(colors =gradientColors))
+            )
+        }
     }
+
+@Preview(showBackground = true)
+@Composable
+fun Picture(){
+    Column(
+        modifier =Modifier
+            .fillMaxWidth()
+            .background(Color.Black)
+    ){
+        Image(
+            modifier= Modifier
+                .fillMaxWidth(),
+            painter = painterResource(R.drawable.android),
+            contentDescription ="Logo Android",
+            contentScale = ContentScale.Crop
+        )
+    }
+
+}
+
+
+fun clickAction(){
+    println("Column Clicked")
+
 }
