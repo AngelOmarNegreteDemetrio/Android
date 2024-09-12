7eb050c806b5bee990f5252a41247f5452a5a2e0
AngelOmarNegreteDemetrio <negretedemetrioangel@gmail.com>
2024-09-11 18:59:39 -0600

    add routing to app crated home and menu Screen

--- app/src/main/java/com/example/prueba/MainActivity.kt
+++ app/src/main/java/com/example/prueba/MainActivity.kt
@@ -1,59 +1,20 @@
 package com.example.prueba
 
-import android.graphics.Picture
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.StringRes
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.VerticalAlignmentLine
-import androidx.compose.ui.modifier.modifierLocalMapOf
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
-import com.example.prueba.ui.theme.PruebaTheme
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.prueba.ui.Screens.MenuScreen
+import com.example.prueba.ui.screens.HomeScreen
+import com.example.prueba.ui.screens.MenuScreen
 
 //import androidx.navigation.compose.NavHostController
 
@@ -62,7 +23,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-ComposeMultiScreen()
+ComposeMultiScreenApp()
            /* Column(
                 modifier= Modifier
                     .fillMaxSize()
@@ -332,21 +293,18 @@ fun clickAction(){
     println("Column Clicked")
 
 }*/
+@Preview
 @Composable
-fun ComposeMultiScreen(){
-    val navController =rememberNavController()
-               Surface(color =Color.White) {
-                   SetupNavGraph(navController = navController)
-               }
-
-           }
-
-            @Composable
-            fun SetupNavGraph(navController: NavHostController){
-                NavHost(
-                    navController= NavController,startDestination ="menu"){
-                    composable("menu"){ MenuScreen(navController)
-                        composable("menu"){ MenuScreen(navController)
-                    }
-                }
-            }
+fun ComposeMultiScreenApp(){
+    val navController = rememberNavController()
+    Surface(color = Color.White) {
+        SetupNavGraph(navController = navController)
+    }
+}
+@Composable
+fun SetupNavGraph(navController: NavHostController){
+    NavHost(navController = navController, startDestination = "menu") {
+        composable("menu") { MenuScreen(navController) }
+        composable("home") { HomeScreen(navController) }
+    }
+}
